926db56fc7d7c5a11c15ca027b1f0aff289cec97
M. Samuel Dennis <32065322+Samuel12390@users.noreply.github.com>
2022-09-22 15:02:19 -0400

    Update Write.py
    Added code for buzzer call on ready to scan and scanned status for RFID cards. Code successfully writes text to a RFID card.

--- Write.py
+++ Write.py
@@ -1,14 +1,34 @@
-#!/usr/bin/env python
-
-import RPi.GPIO as GPIO
+import sys
 from mfrc522 import SimpleMFRC522
+from time import sleep
+import RPi.GPIO as GPIO
+
+#Disable warnings (optional)
+GPIO.setwarnings(False)
+
+#Select GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+#Set buzzer GPIO Pin 4 as output
+buzzer = 4
+GPIO.setup(buzzer,GPIO.OUT)
 
 reader = SimpleMFRC522()
-
 try:
         text = input('New Data:')
-        print("Now place your tag to write")
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.LOW)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.LOW)
+        print("Now place your tag to write...")
         reader.write(text)
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.5)
+        GPIO.output(buzzer,GPIO.LOW)
         print("Written")
 finally:
         GPIO.cleanup() 
+
