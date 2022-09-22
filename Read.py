83025f5a3f0fdd210b58257793f61a23979589d1
M. Samuel Dennis <32065322+Samuel12390@users.noreply.github.com>
2022-09-22 14:56:43 -0400

    Update Read.py
    Added code for the working buzzer for both ready and scanned sounds, as well as an update to the Raspberry Pi system. This code successfully reads the information on both a blank & overwritten RFID card/tag

--- Read.py
+++ Read.py
@@ -3,14 +3,34 @@ from mfrc522 import SimpleMFRC522
 from time import sleep
 import RPi.GPIO as GPIO
 
+#Disable warnings (optional)
+GPIO.setwarnings(False)
+
+#Select GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+#Set buzzer GPIO Pin 4 as output
+buzzer = 4
+GPIO.setup(buzzer,GPIO.OUT)
+
 reader = SimpleMFRC522()
 
 try:
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.LOW)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.LOW)
         print("Hold your tag near the reader...")
         id, text = reader.read()
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.5)
+        GPIO.output(buzzer,GPIO.LOW)
         print("ID: %s\nTest: %s" % (id, text))
-        sleep(5)
         
 except KeyboardInterrupt:
         GPIO.cleanup()
         print("Exiting program.")
+        
