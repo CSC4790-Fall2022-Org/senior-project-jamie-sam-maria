aa8a58fe5a2f851d744b2373eaec0d99b5c53f3a
M. Samuel Dennis <32065322+Samuel12390@users.noreply.github.com>
2022-12-08 15:21:33 -0500

    Update Read.py
    Add buzzer functionality

--- Read.py
+++ Read.py
@@ -1,14 +1,39 @@
 import sys
+import drivers
 from mfrc522 import SimpleMFRC522
 from time import sleep
 import RPi.GPIO as GPIO
 
-reader = SimpleMFRC522()
+#Disable warnings (optional)
+GPIO.setwarnings(False)
 
+#Select GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+#Set buzzer GPIO Pin 4 as output
+buzzer = 4
+GPIO.setup(buzzer,GPIO.OUT)
+
+#Set up LCD screen for display use
+display = drivers.Lcd()
+display.lcd_clear()
+
+reader = SimpleMFRC522()
 try:
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.LOW)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.1)
+        GPIO.output(buzzer,GPIO.LOW)
+        display.lcd_clear()
         print("Hold your tag near the reader...")
         id, text = reader.read()
-        print("ID: %s\nTest: %s" % (id, text))
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(0.5)
+        GPIO.output(buzzer,GPIO.LOW)
+        print("ID: %s\nUsername: %s" % (id, text))
         sleep(5)
         
 except KeyboardInterrupt:
